2bd9ce0f1d4fda51c9a31e641baaf764cfc05f3a
Ammar-Amin <ammaramin5253@gmail.com>
2025-11-02 12:52:33 +0530

    feat: project setup & add Navbar component

---
+++ src/constants/index.ts
@@ -0,0 +1,26 @@
+export const navLinks = [
+  {
+    label: "Store",
+  },
+  {
+    label: "Mac",
+  },
+  {
+    label: "iPad",
+  },
+  {
+    label: "iPhone",
+  },
+  {
+    label: "Watch",
+  },
+  {
+    label: "AirPods",
+  },
+  {
+    label: "Accessories",
+  },
+  {
+    label: "Support",
+  },
+];
